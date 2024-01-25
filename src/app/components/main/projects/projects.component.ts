b5d795b64e315d7f68577f7d445a663cf40a6dca
Mitesh Mor <miteshmor47@gmail.com>
2024-01-25 16:10:27 +0530

    Almost finished portfolio

--- src/app/components/main/projects/projects.component.ts
+++ src/app/components/main/projects/projects.component.ts
@@ -7,7 +7,7 @@ interface Projects {
   ghLink?: string;
   Technologies: string[];
 }
-
+declare const bootstrap: any;
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -16,9 +16,13 @@ interface Projects {
 export class ProjectsComponent implements OnInit {
   @ViewChild('imgContainer') imgContainer!: ElementRef;
 
-  constructor() {}
+  constructor(private elementRef: ElementRef) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const carousels =
+      this.elementRef.nativeElement.querySelectorAll('.carousel');
+    carousels.forEach((carousel: any) => new bootstrap.Carousel(carousel));
+  }
 
   debug() {
     this.imgContainer.nativeElement.scroll({
@@ -46,11 +50,14 @@ export class ProjectsComponent implements OnInit {
     {
       Title: 'Cric-Score',
       Description:
-        '▹ Web app which shows cricket score with scorecard, commentary, and partnership between two batsman.<br> ▹ Includes standalone components and inlie styles and template. <br> ▹ Used Angular Material for the designing and custom services for scoring random runs. ',
+        '▹ Web app which shows cricket score with scorecard, commentary, and partnership between two batsman.<br> ▹ Includes standalone components and inline styles and template. <br> ▹ Used Angular Material for the designing and custom services for scoring random runs. ',
       imgs: [
-        'assets/images/threads1.png',
-        'assets/images/threads2.png',
-        'assets/images/threads3.png',
+        'assets/images/cricscore1.png',
+        'assets/images/cricscore2.png',
+        'assets/images/cricscore3.png',
+        'assets/images/cricscore4.png',
+        'assets/images/cricscore5.png',
+        'assets/images/cricscore6.png',
       ],
       Technologies: ['Angular', 'Angular Material', 'Bootstrap 5', 'SCSS'],
       ghLink: 'https://github.com/Mitesh47/Cric-Score',
@@ -58,11 +65,18 @@ export class ProjectsComponent implements OnInit {
     {
       Title: 'Real-time Chat App (WebApp)',
       Description:
-        '▹ Chat App is a web application that helps you to communicate with any user in the app. <br> ▹ Once the user is logged in he/she will be available in the chat list.  <br> ▹ WebApp is developed using Angular, NestJS  and Mysql for database. <br> ▹ Used Firebase JWT token for authorization of user. ',
+        '▹ Chat App is a web application that helps you to communicate with any user in the app. <br> ▹ Once the user is logged in he/she will be available in the chat list.  <br> ▹ WebApp is developed using Angular, NestJS  and MySql for database. <br> ▹ Used Firebase JWT token for authorization of user. ',
       imgs: [
-        'assets/images/threads1.png',
-        'assets/images/threads2.png',
-        'assets/images/threads3.png',
+        'assets/images/chatapp1.png',
+        'assets/images/chatapp2.png',
+        'assets/images/chatapp3.png',
+        'assets/images/chatapp4.png',
+        'assets/images/chatapp5.png',
+        'assets/images/chatapp6.png',
+        'assets/images/chatapp7.png',
+        'assets/images/chatapp8.png',
+        'assets/images/chatapp9.png',
+        'assets/images/chatapp10.png',
       ],
       Technologies: ['Angular', 'Angular Material', 'NestJS', 'MySQL'],
       ghLink: 'https://github.com/Mitesh47/nest-update-profile',
@@ -72,9 +86,14 @@ export class ProjectsComponent implements OnInit {
       Description:
         '▹ Chat App is a web application that helps you to communicate with any user in the app. <br> ▹ Once the user is logged in he/she will be available in the chat list.  <br> ▹ WebApp is developed using Angular, NestJS  and Mysql for database. <br> ▹ Used Firebase JWT token for authorization of user. ',
       imgs: [
-        'assets/images/threads1.png',
-        'assets/images/threads2.png',
-        'assets/images/threads3.png',
+        'assets/images/mattable1.png',
+        'assets/images/mattable2.png',
+        'assets/images/mattable3.png',
+        'assets/images/mattable4.png',
+        'assets/images/mattable5.png',
+        'assets/images/mattable6.png',
+        'assets/images/mattable7.png',
+        'assets/images/mattable8.png',
       ],
       Technologies: ['Angular', 'Angular Material', 'NestJS', 'MySQL'],
       ghLink: 'https://github.com/Mitesh47/nest-update-profile',
